b8cbdcc6c39fab90478bf1804c4206e8fa42690b
Sergey Bespalov <sergey.bespalov@protonmail.com>
2020-06-02 01:09:30 +0300

    artist api post

--- api/artists.js
+++ api/artists.js
@@ -20,12 +20,58 @@ artistsRouter.param('artistId', (req, res, next, id) => {
     if (error) {
       next(error)
     } else if (row) {
-      req.artistId = id
+      req.artistId = row.id
+      req.artist = row
       next()
     } else {
       res.sendStatus(404)
     }
   })
+})
+
+artistsRouter.get('/:artistId', (req, res, next) => {
+  res.status(200).json({ artist: req.artist })
+})
+
+artistsRouter.post('/', (req, res, next) => {
+  const artist = req.body.artist
+  const valid = artist.name && artist.dateOfBirth && artist.biography
+  console.log(artist.name, artist.dateOfBirth, artist.biography)
+  if (valid) {
+    if (!artist.is_currently_employed) {
+      artist.is_currently_employed = 1
+    }
+    db.run(`
+      INSERT INTO Artist 
+        (
+          name, 
+          date_of_birth, 
+          biography, 
+          is_currently_employed
+        )
+      VALUES
+        (
+          "${artist.name}",
+          "${artist.dateOfBirth}",
+          "${artist.biography}",
+          ${artist.is_currently_employed}
+        )
+    `, function (error) {
+      if (error) {
+        next(error)
+      } else {
+        db.get('SELECT * FROM Artist WHERE id = $id', { $id: this.lastID }, (error, row) => {
+          if (error) {
+            next(error)
+          } else if (row) {
+            res.status(201).json({ artist: row })
+          }
+        })
+      }
+    })
+  } else {
+    res.sendStatus(400)
+  }
 })
 
 module.exports = artistsRouter
