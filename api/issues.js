d6b4223e431cf3734ca261e943c6dfd41dfe4aa7
Sergey Bespalov <sergey.bespalov@protonmail.com>
2020-06-12 13:22:24 +0300

    issues api post

--- api/issues.js
+++ api/issues.js
@@ -17,4 +17,45 @@ issuesRouter.get('/', (req, res, next) => {
   )
 })
 
+issuesRouter.post('/', (req, res, next) => {
+  const issue = req.body.issue
+  const valid = issue.name && issue.issueNumber && issue.publicationDate && issue.artistId
+  if (!valid) {
+    console.log('here', issue, valid)
+    return res.sendStatus(400)
+  }
+  db.get(
+    'SELECT * FROM Artist WHERE id = $id',
+    { $id: issue.artistId },
+    (error, row) => {
+      if (error) {
+        return next(error)
+      }
+      if (!row) {
+        return res.sendStatus(400)
+      }
+      db.run(`
+        INSERT INTO Issue (
+          name, issue_number, publication_date, artist_id, series_id
+        )
+        VALUES (
+          "${issue.name}", ${issue.issueNumber}, "${issue.publicationDate}", ${issue.artistId}, ${req.series.id}
+        )
+      `,
+      function(error) {
+        if (error) {
+          return next(error)
+        }
+        db.get('SELECT * FROM Issue WHERE id = $id', { $id: this.lastID }, (error, row) => {
+          if (error) {
+            return next(error)
+          }
+          res.status(201).json({ issue: row })
+        })
+      }
+      )
+    }
+  )
+})
+
 module.exports = issuesRouter
